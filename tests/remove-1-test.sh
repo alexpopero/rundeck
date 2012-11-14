b5f9714652aa2d277aaa72c2ff106dfca20f1f64
connaryscott <connaryscott@gmail.com>
2012-11-13 17:08:32 -0800

    adjust to correctly maintain /var/rundeck directory

--- tests/remove-1-test.sh
+++ tests/remove-1-test.sh
@@ -13,17 +13,21 @@ describe "remove"
 
 it_runs_without_arguments() {
     rerun rundeck:install
+    rerun rundeck:stop
     rerun rundeck:remove
 
     test -d /var/lib/rundeck  && { true; } || { false; }
+    test -d /var/rundeck  && { true; } || { false; }
 }
 
 it_runs_with_cleanup() {
     rerun rundeck:install
+    rerun rundeck:stop
     rerun rundeck:remove --cleanup true
 
     test -e /var/log/rundeck && { false; } || { true; }
     test -e /var/lib/rundeck  && { false; } || { true; }
+    test -e /var/rundeck  && { false; } || { true; }
 
 }
 
